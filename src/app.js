f27c8b6dac8ce4a08f760818368571b7d06b4cda
JosuGoiris <63734391+JosuGoiris@users.noreply.github.com>
2022-02-15 15:42:12 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
+import morgan from "morgan";
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  engine({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", ".hbs");
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}))
+
+app.use(indexRoutes);
+
+export default app;
---
+++ src/routes/index.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import Task from "../models/Task";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
+
+  res.render("index", { tasks: tasks });
+});
+
+router.post("/task/add", async (req, res) => {
+  try {
+    const task = Task(req.body);
+
+    await task.save();
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+export default router;
